5bed82ca6a081091eae0c7db07a6e669f8ccb7de
Viet Anh Tran <viet.anhtran@myob.com>
2018-11-16 15:43:18 +1300

    EC2 instance inside subnet without public ip

--- ops/terraform/ec2.tf
+++ ops/terraform/ec2.tf
@@ -12,3 +12,18 @@ resource "aws_instance" "internet-vpc-bastion" {
     Name = "internet-vpc-bastion"
   }
 }
+
+resource "aws_instance" "ec2-with-internet" {
+   ami  = "${var.ami}"
+   instance_type = "t1.micro"
+   key_name = "${aws_key_pair.default.id}"
+   subnet_id = "${aws_subnet.public-subnet.id}"
+   vpc_security_group_ids = ["${aws_security_group.internet-vpc-bastion-sg.id}"]
+   associate_public_ip_address = false
+   source_dest_check = false
+   user_data = "${file("install.sh")}"
+
+  tags {
+    Name = "ec2-with-internet"
+  }
+}
