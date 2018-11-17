04e3f3329a2a519da371abb0f691c0fe8662b735
vietanhtran1996 <tranvietanh1996@gmail.com>
2018-11-17 18:38:56 +1300

    Ignore changes in user data for ec2 instances

--- ops/terraform/ec2.tf
+++ ops/terraform/ec2.tf
@@ -1,28 +1,34 @@
 resource "aws_instance" "internet-vpc-bastion" {
-   ami  = "${var.ami}"
-   instance_type = "t1.micro"
-   key_name = "${aws_key_pair.default.id}"
-   subnet_id = "${aws_subnet.public-subnet.id}"
-   vpc_security_group_ids = ["${aws_security_group.internet-vpc-bastion-sg.id}"]
-   associate_public_ip_address = true
-   source_dest_check = false
-   user_data = "${file("install.sh")}"
+  ami  = "${var.ami}"
+  instance_type = "t1.micro"
+  key_name = "${aws_key_pair.default.id}"
+  subnet_id = "${aws_subnet.public-subnet.id}"
+  vpc_security_group_ids = ["${aws_security_group.internet-vpc-bastion-sg.id}"]
+  associate_public_ip_address = true
+  source_dest_check = false
+  user_data = "${file("install.sh")}"
 
+  lifecycle {
+    ignore_changes = ["user_data"]
+  } 
   tags {
     Name = "internet-vpc-bastion"
   }
 }
 
 resource "aws_instance" "ec2-with-internet" {
-   ami  = "${var.ami}"
-   instance_type = "t1.micro"
-   key_name = "${aws_key_pair.default.id}"
-   subnet_id = "${aws_subnet.public-subnet.id}"
-   vpc_security_group_ids = ["${aws_security_group.internet-vpc-bastion-sg.id}"]
-   associate_public_ip_address = false
-   source_dest_check = false
-   user_data = "${file("install.sh")}"
+  ami  = "${var.ami}"
+  instance_type = "t1.micro"
+  key_name = "${aws_key_pair.default.id}"
+  subnet_id = "${aws_subnet.public-subnet.id}"
+  vpc_security_group_ids = ["${aws_security_group.internet-vpc-bastion-sg.id}"]
+  associate_public_ip_address = false
+  source_dest_check = false
+  user_data = "${file("install.sh")}"
 
+  lifecycle {
+    ignore_changes = ["user_data"]
+  } 
   tags {
     Name = "ec2-with-internet"
   }
