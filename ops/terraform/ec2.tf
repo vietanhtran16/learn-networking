fe1a5df9791541cc3355cabe40d1a571fd35e395
vietanhtran1996 <tranvietanh1996@gmail.com>
2018-11-22 22:12:33 +1300

    WIP

--- ops/terraform/ec2.tf
+++ ops/terraform/ec2.tf
@@ -7,6 +7,7 @@ resource "aws_instance" "internet-vpc-bastion" {
   associate_public_ip_address = true
   source_dest_check = false
   user_data = "${file("install.sh")}"
+  availability_zone = "ap-southeast-2a"
 
   lifecycle {
     ignore_changes = ["user_data"]
@@ -22,9 +23,10 @@ resource "aws_instance" "ec2-with-internet" {
   key_name = "${aws_key_pair.default.id}"
   subnet_id = "${aws_subnet.public-subnet.id}"
   vpc_security_group_ids = ["${aws_security_group.internet-vpc-bastion-sg.id}"]
-  associate_public_ip_address = false
+  
   source_dest_check = false
   user_data = "${file("install.sh")}"
+  availability_zone = "ap-southeast-2a"
 
   lifecycle {
     ignore_changes = ["user_data"]
--- ops/terraform/internet-vpc.tf
+++ ops/terraform/internet-vpc.tf
@@ -1,6 +1,7 @@
 resource "aws_vpc" "vpc-with-internet" {
   cidr_block = "${var.internet_vpc_cidr}"
-  enable_dns_hostnames = true
+  enable_dns_hostnames = false
+  enable_dns_support = true
 
   tags {
     Name = "viet-vpc-with-internet"
@@ -19,19 +20,27 @@ resource "aws_subnet" "public-subnet" {
   vpc_id     = "${aws_vpc.vpc-with-internet.id}"
   cidr_block = "10.0.8.0/24"
   map_public_ip_on_launch = true
+  availability_zone = "ap-southeast-2a"
 
   tags {
     Name = "public-subnet"
   }
 }
 
-resource "aws_route" "internet_access" {
-  route_table_id         = "${aws_vpc.vpc-with-internet.main_route_table_id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.internet-gateway.id}"
+resource "aws_route_table" "public-subnet-rt" {
+  vpc_id = "${aws_vpc.vpc-with-internet.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.internet-gateway.id}"
+  }
+
+  tags {
+    Name = "Public Subnet RT"
+  }
 }
 
-resource "aws_route_table_association" "route-table-association" {
+resource "aws_route_table_association" "web-public-rt" {
   subnet_id = "${aws_subnet.public-subnet.id}"
-  route_table_id = "${aws_route_table.public-subnet-route-table.id}"
+  route_table_id = "${aws_route_table.public-subnet-rt.id}"
 }
